ae4a46976efe435870efd1869e43ecbb7cf14a0e
Maike22-stra <xumeieaster@gmail.com>
2026-05-09 02:14:29 -0700

    Create i18n.ts

---
+++ lib/i18n.ts
@@ -0,0 +1,11 @@
+export const translations = {
+  en: {
+    hero: "Direct Airline Cadet Program",
+  },
+  zh: {
+    hero: "航司直入飞行员计划",
+  },
+  fr: {
+    hero: "Programme Cadet Direct Compagnie Aérienne",
+  },
+}
